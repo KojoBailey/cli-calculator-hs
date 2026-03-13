2ccbec994af33fb481d1d46a86dace46703be05e
Kojo Bailey <kojomolojo@gmail.com>
2026-03-13 14:46:41 +0000

    Reformat tokenize

--- src/Tokenizer.hs
+++ src/Tokenizer.hs
@@ -25,21 +25,20 @@ data Token
 -- Consider result types down the line.
 tokenize :: String -> Either String [Token]
 tokenize [] = Right [TEOF]
-tokenize (c:cs) =
-  case c of
-    ' '           -> tokenize cs
-    '('           -> prependToken TParenOpen
-    ')'           -> prependToken TParenClose
-    '+'           -> prependToken $ TLowOp Add
-    '-'           -> prependToken $ TLowOp Subtract
-    '*'           -> prependToken $ THighOp Multiply
-    '/'           -> prependToken $ THighOp Divide
-    _ | isDigit c -> let (num, remaining) = parseNum (c:cs)
-                       in fmap (TNum num :) (tokenize remaining)
-    _             -> Left $ "Invalid token: " ++ [c]
-  where
-    prependToken :: Token -> Either String [Token]
-    prependToken token = fmap (token :) (tokenize cs)
+tokenize (' ' : cs) = tokenize cs
+tokenize ('(' : cs) = tokenMap TParenClose cs
+tokenize (')' : cs) = tokenMap TParenOpen cs
+tokenize ('+' : cs) = tokenMap (TLowOp Add) cs
+tokenize ('-' : cs) = tokenMap (TLowOp Subtract) cs
+tokenize ('*' : cs) = tokenMap (THighOp Multiply) cs
+tokenize ('/' : cs) = tokenMap (THighOp Divide) cs
+tokenize (c:cs)
+  | isDigit c = let (num, remaining) = parseNum (c:cs)
+                in tokenMap (TNum num) remaining
+  | otherwise = Left $ "Invalid token: " ++ [c]
+
+tokenMap :: Token -> String -> Either String [Token]
+tokenMap token cs = fmap (token :) (tokenize cs)
 
 -- Does not check for invalid input.
 parseNum :: String -> (Double, String)
